c4f809418ab72353b5f5a143817ed9dc426f5703
github nitf <2tf2003@gmail.com>
2019-07-18 11:05:28 +0900

    add: Widget

---
+++ lib/widget/ColorButton.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class ColorButton extends StatelessWidget {
+  final Color color;
+  final double width;
+  final double height;
+
+  final Color border_color;
+  final double border_width;
+
+  ColorButton({
+    Key key,
+    this.color,
+    this.width,
+    this.height,
+    this.border_color = Colors.white,
+    this.border_width = 0.0}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+        onPressed: null,
+        constraints: BoxConstraints(minWidth: 0.0, minHeight: 0.0),
+        child: Container(
+          margin: EdgeInsets.only(top: 3.0, bottom: 3.0, left: 1.0, right: 1.0),
+          width: this.width,
+          height: this.height,
+          decoration: BoxDecoration(
+              color: this.color,
+              borderRadius: BorderRadius.all(Radius.circular(25.0)),
+              border: Border.all(color: this.border_color, width: this.border_width)
+          ),
+        )
+    );
+  }
+}
