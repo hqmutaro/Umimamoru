a3092ad1d39b58d0b34ec9be301a88b0b2b68be5
github nitf <2tf2003@gmail.com>
2019-07-19 11:58:30 +0900

    add: dependcy

--- android/app/src/main/kotlin/umimamoruflutter/nitf/github/com/umimamoru/MainActivity.kt
+++ android/app/src/main/kotlin/umimamoruflutter/nitf/github/com/umimamoru/MainActivity.kt
@@ -1,7 +1,6 @@
-package umimamoruflutter.nitf.github.com.umimamoru_flutter
+package umimamoruflutter.nitf.github.com.umimamoru
 
 import android.os.Bundle
-
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
