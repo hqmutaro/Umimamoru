ef5ae623cd4671160ff3e2825a795b0dbb6d5ca7
github nitf <2tf2003@gmail.com>
2019-07-23 08:36:01 +0900

    feat: class拡張

--- lib/widget/color_button.dart
+++ lib/widget/color_button.dart
@@ -8,20 +8,26 @@ class ColorButton extends StatelessWidget {
   final Color border_color;
   final double border_width;
 
+  final pressed;
+  final Icon icon;
+
   ColorButton({
     Key key,
     this.color,
     this.width,
     this.height,
     this.border_color = Colors.white,
-    this.border_width = 0.0}) : super(key: key);
+    this.border_width = 0.0,
+    this.pressed = null,
+    this.icon = null}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return RawMaterialButton(
-        onPressed: null,
+        onPressed: this.pressed,
         constraints: BoxConstraints(minWidth: 0.0, minHeight: 0.0),
         child: Container(
+          child: this.icon,
           margin: EdgeInsets.only(top: 3.0, bottom: 3.0, left: 1.0, right: 1.0),
           width: this.width,
           height: this.height,
@@ -30,7 +36,7 @@ class ColorButton extends StatelessWidget {
               borderRadius: BorderRadius.all(Radius.circular(25.0)),
               border: Border.all(color: this.border_color, width: this.border_width)
           ),
-        )
+        ),
     );
   }
 }
