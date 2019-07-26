6dbacf98d3f79fc60392784908f3d4a5c1b9ee2d
github nitf <2tf2003@gmail.com>
2019-07-26 11:22:16 +0900

    add: ビーチ名と地域名の表示

---
+++ lib/presentation/info/beach_name.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class BeachName extends StatelessWidget {
+
+  String region;
+  String beach;
+
+  BeachName({
+    @required this.region,
+    @required this.beach,
+  }) : assert(region != null),
+        assert(beach != null);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Text(
+          this.region + "\n" +
+          this.beach + "の波のようす",
+          style: TextStyle(
+              fontSize: 23.0,
+              fontWeight: FontWeight.bold
+          )
+        )
+    );
+  }
+}
