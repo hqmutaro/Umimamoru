d353f3e29e744cbc0fe8d0749580ed6f7b453d67
github nitf <2tf2003@gmail.com>
2019-07-26 11:23:23 +0900

    add: 設定画面

---
+++ lib/presentation/config/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:umimamoru_flutter/presentation/config/state.dart';
+
+// ignore: must_be_immutable
+class ConfigDisplay extends StatefulWidget {
+
+  ConfigDisplay({Key key}) : super(key: key);
+
+  @override
+  ConfigState createState() => ConfigState();
+}
