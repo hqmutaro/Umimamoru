e4b63637e3bea4afc83db83855f35e5cbb1665ec
github nitf <2tf2003@gmail.com>
2019-07-31 22:30:44 +0900

    remove: ミーティングの必要あり
    Server関連は後回しに
    非同期処理の勉強をする

--- lib/application/repository/cone_state_repository.dart
+++ lib/application/repository/cone_state_repository.dart
@@ -1,25 +1,4 @@
-import 'package:flutter/material.dart';
-import 'package:umimamoru_flutter/domain/cone_state.dart';
 import 'package:umimamoru_flutter/application/repository/repository.dart';
-import 'package:umimamoru_flutter/infrastructure/server/data_task.dart';
-import 'package:umimamoru_flutter/domain/entity.dart';
 
-class ConeStateRepository extends Repository{
-
-  Entity entity;
-  String beach;
-  DataTask task;
-
-  ConeStateRepository(@required beach) : assert(beach != null) {
-    this.task = DataTask(beach);
-  }
-
-  void setEntity(Entity entity) {
-    this.entity = entity;
-  }
-
-  List<ConeState> getEntities() {
-    this.task.sendToEntity();
-    return this.task.getResult();
-  }
+class ConeStateRepository implements Repository {
 }
