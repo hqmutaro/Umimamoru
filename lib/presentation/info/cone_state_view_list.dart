a0b3b24aa5008a8e1e86585698d133eca4a647ff
github nitf <2tf2003@gmail.com>
2019-08-01 10:23:43 +0900

    feat: ステートのload中はLoaderを描画する

--- lib/presentation/info/cone_state_view_list.dart
+++ lib/presentation/info/cone_state_view_list.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:umimamoru_flutter/application/repository/cone_state_repository.dart';
 import 'package:umimamoru_flutter/presentation/info/cone_state_view.dart';
 import 'package:umimamoru_flutter/domain/entity.dart';
+import 'package:umimamoru_flutter/presentation/loader.dart';
 
 class ConeStateViewList extends StatelessWidget {
 
@@ -12,6 +13,12 @@ class ConeStateViewList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     List<Widget> widgets = [];
+    if (this.entities.isEmpty) {
+      return Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Loader(colors: [Colors.red, Colors.blue], duration: Duration(milliseconds: 1000))
+      );
+    }
 
     this.entities.forEach((cone, entity) => widgets.add(ConeStateView(cone: cone, entity: entity)));
 
