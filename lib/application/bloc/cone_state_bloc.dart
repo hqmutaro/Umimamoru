b6dad84b6e17e6e40d32ec636430ceea8c20f6fd
github nitf <2tf2003@gmail.com>
2019-08-14 16:47:56 +0900

    add: BLoC
    BLoC Base
    BLoC Provider
    BLoC ConeState
    BLoC OccurCone

---
+++ lib/application/bloc/cone_state_bloc.dart
@@ -0,0 +1,70 @@
+import 'dart:async';
+import 'package:umimamoru/application/bloc/bloc_base.dart';
+import 'package:umimamoru/domain/cone_state.dart';
+import 'package:umimamoru/domain/wave_speed.dart';
+
+class ConeStateBloc extends BlocBase {
+
+  StreamController<void> _startController = StreamController<void>();
+  StreamController<List<ConeState>> _outputController = StreamController<List<ConeState>>();
+
+  StreamSink<void> get start => _startController.sink;
+
+  Stream<List<ConeState>> get output => _outputController.stream;
+
+  String beach;
+
+  ConeStateBloc(String beach) {
+    this.beach = beach;
+    _startController.stream.listen((_) {
+      _start();
+    });
+  }
+
+  void _start() {
+    var stream = Stream.periodic(const Duration(seconds: 5), (count) {
+      return {
+        "1番コーン": {
+          "wave.speed": 0.3,
+          "count.occur": 3
+        },
+        "2番コーン": {
+          "wave.speed": 0.5,
+          "count.occur": 1
+        },
+        "3番コーン": {
+          "wave.speed": 0.8,
+          "count.occur": 4
+        },
+        "4番コーン": {
+          "wave.speed": 1.4,
+          "count.occur": 3
+        },
+        "5番コーン": {
+          "wave.speed": 1.8,
+          "count.occur": 2
+        }
+      };
+    });
+    stream.listen((result) {
+      List<ConeState> entities = [];
+      result.forEach((cone, data) =>
+          entities.add(
+              ConeState(
+                  this.beach,
+                  cone,
+                  getLevelToString(getLevel(data["wave.speed"])),
+                  data["wave.speed"],
+                  data["count.occur"]
+              )
+          ));
+      _outputController.sink.add(entities);
+    });
+  }
+
+  @override
+  void dispose() async{
+    await _startController.close();
+    await _outputController.close();
+  }
+}
---
+++ lib/application/bloc/occur_cone_bloc.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+import 'package:umimamoru/application/bloc/bloc_base.dart';
+
+class OccurConeBloc extends BlocBase {
+
+  StreamController<void> _startController = StreamController<void>();
+  StreamController<List<String>> _outputController = StreamController<List<String>>();
+
+  StreamSink<void> get start => _startController.sink;
+  Stream<List<String>> get output => _outputController.stream;
+
+  String beach;
+
+  OccurConeBloc(String beach) {
+    this.beach = beach;
+    _startController.stream.listen((_) {
+      _start();
+    });
+  }
+
+  void _start() {
+    var stream = Stream.periodic(const Duration(seconds: 5), (count) {
+      return  [
+        "2番コーン",
+        "3番コーン"
+      ];
+    });
+    stream.listen((result) {
+      _outputController.sink.add(result);
+    });
+  }
+
+  @override
+  void dispose() async{
+    await _startController.close();
+    await _outputController.close();
+  }
+
+}
