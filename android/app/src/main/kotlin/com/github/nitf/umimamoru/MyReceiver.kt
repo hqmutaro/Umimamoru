2dddcb48148ace7409b9221574139b67c42769c1
github nitf <2tf2003@gmail.com>
2019-09-16 11:26:52 +0900

    add: ActionImages

--- android/app/src/main/kotlin/com/github/nitf/umimamoru/MyReceiver.kt
+++ android/app/src/main/kotlin/com/github/nitf/umimamoru/MyReceiver.kt
@@ -5,7 +5,6 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 
-import io.flutter.app.FlutterActivity
 import io.flutter.plugin.common.MethodChannel
 
 class MyReceiver(private val methodChannel: MethodChannel): BroadcastReceiver() {
