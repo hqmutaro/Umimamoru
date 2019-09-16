2aed7c9084780653885b5c31c949e9fca3ddb7cd
github nitf <2tf2003@gmail.com>
2019-09-16 11:29:53 +0900

    feat: Provider initialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:umimamoru/infrastructure/server/check_state.dart';
+import 'package:umimamoru/infrastructure/service/check_state.dart';
+import 'package:umimamoru/infrastructure/service/occurring_manager.dart';
+import 'package:umimamoru/infrastructure/service/watch_provider.dart';
 import 'package:umimamoru/presentation/app.dart';
 
-void main() {
-  var checkState = CheckState();
-  var methodChannel = const MethodChannel("com.github.nitf");
-  methodChannel.setMethodCallHandler((call) async{
-    checkState.checkState();
-  });
+void main() async{
+  await WatchProvider().init();
+  await OccurringManager().init();
+  var provider = WatchProvider.getInstance();
+  //await provider.addBeach("1919");
+  var watches = await provider.getWatches();
+  watches.forEach((watch) => print(watch));
   runApp(App());
 }
