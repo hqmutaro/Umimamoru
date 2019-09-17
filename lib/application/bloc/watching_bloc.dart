77ff3dcf1aac8e744670de1f1f5727f58735cef7
github nitf <2tf2003@gmail.com>
2019-09-18 01:32:23 +0900

    add: WatchingBloc

---
+++ lib/application/bloc/watching_bloc.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:umimamoru/application/bloc/bloc_base.dart';
+import 'package:umimamoru/infrastructure/service/watch_provider.dart';
+
+class WatchingBloc extends BlocBase {
+
+  StreamController<void> _startController = StreamController<void>();
+  StreamController<bool> _outputController = StreamController<bool>();
+
+  StreamSink<void> get start => _startController.sink;
+  Stream<bool> get output => _outputController.stream;
+
+  String beach;
+
+  WatchingBloc(String beach) {
+    this.beach = beach;
+    _startController.stream.listen((_) => _start());
+  }
+
+  void _start() {
+    var provider = WatchProvider.getInstance();
+    provider.existsBeach(this.beach)
+        .then((result) =>
+        _outputController.sink.add(result)
+    );
+  }
+
+  @override
+  void dispose() async{
+    await _startController.close();
+    await _outputController.close();
+  }
+}
