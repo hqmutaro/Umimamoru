eda690368e947d2b9a057ccb30e39f8ea2b3e8c2
github nitf <2tf2003@gmail.com>
2019-10-08 22:52:10 +0900

    fix: Poleのfieldにアクセス

--- lib/presentation/display/info/occur_pole_view.dart
+++ lib/presentation/display/info/occur_pole_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:umimamoru/application/bloc/bloc_provider.dart';
 import 'package:umimamoru/application/bloc/occur_pole_bloc.dart';
+import 'package:umimamoru/domain/pole.dart';
 
 @immutable
 class OccurPoleView extends StatelessWidget {
@@ -14,15 +15,15 @@ class OccurPoleView extends StatelessWidget {
     final bloc = BlocProvider.of<OccurPoleBloc>(context);
     bloc.start.add(null);
 
-    return StreamBuilder<List<String>>(
+    return StreamBuilder<List<Pole>>(
       stream: bloc.output,
-      builder: (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
+      builder: (BuildContext context, AsyncSnapshot<List<Pole>> snapshot) {
         if (snapshot.hasData) {
           List<Widget> widgets = [];
-          snapshot.data.forEach((cone) => widgets.add(Padding(
+          snapshot.data.forEach((pole) => widgets.add(Padding(
               padding: EdgeInsets.only(left: 20.0),
               child: Text(
-                  "・$cone番ポール",
+                  "・${pole.loc}番ポール",
                   textAlign: TextAlign.left,
                   style: TextStyle(
                       fontSize: 20.0,
