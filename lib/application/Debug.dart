a56fa2d023d78a1b8a2a7e31e6ac4c45ef201adf
github nitf <2tf2003@gmail.com>
2019-10-08 22:47:14 +0900

    fix: unDebugMode

--- lib/application/Debug.dart
+++ lib/application/Debug.dart
@@ -2,7 +2,9 @@ class Debug {
 
   static bool _isDebugMode = false;
 
-  static void setDebugMode() => _isDebugMode = true;
+  static void onDebugMode() => _isDebugMode = true;
+
+  static void unDebugMode() => _isDebugMode = false;
 
   static bool isDebugMode() => _isDebugMode;
 }
