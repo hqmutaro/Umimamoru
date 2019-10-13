e8d0ea45932e3ddc3ad4cb6b5eaa1d08a295188b
github nitf <2tf2003@gmail.com>
2019-10-14 01:51:57 +0900

    fix: 離岸流を検知したらリアルタイムで画像変更する

--- lib/application/bloc/image_action_bloc.dart
+++ lib/application/bloc/image_action_bloc.dart
@@ -1,8 +1,12 @@
 import 'dart:async';
+import 'dart:convert';
 import 'package:umimamoru/application/bloc/bloc_base.dart';
 import 'package:umimamoru/domain/beach.dart';
+import 'package:umimamoru/domain/module.dart';
 import 'package:umimamoru/domain/wave_speed.dart';
+import 'package:umimamoru/infrastructure/repository/dto/module_dto.dart';
 import 'package:umimamoru/infrastructure/repository/server_module_repository.dart';
+import 'package:umimamoru/infrastructure/repository/server_provider.dart';
 
 class ImageActionBloc extends BlocBase {
 
@@ -19,19 +23,23 @@ class ImageActionBloc extends BlocBase {
     _startController.stream.listen((_) => _start());
   }
 
-  void _start() {
+  void _start() async{
     bool isOccurring = false;
-    var repository = ServerModuleRepository();
-    repository.moduleState(this.beach.name).then((repository) {
-      repository.forEach((module) {
+    ServerModuleRepository repository = ServerModuleRepository();
+
+    var stream = Stream.periodic(const Duration(seconds: 5), (count) async{
+      List<Module> moduleState = await repository.moduleState(this.beach.name);
+      return moduleState;
+    });
+    stream.listen((result) => result.then((moduleList) {
+      moduleList.forEach((module) {
         if (module.level == getLevelToString(Level.Fast)) {
           isOccurring = true;
           _outputController.sink.add(isOccurring);
           return;
         }
       });
-      _outputController.sink.add(isOccurring);
-    });
+    }));
   }
 
   @override
